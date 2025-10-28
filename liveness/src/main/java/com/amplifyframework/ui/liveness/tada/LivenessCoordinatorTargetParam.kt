6a6ea7307659d7b1064d5999a5b101465ce0d191
Nick <gsk5787@gmail.com>
2025-10-28 17:46:52 +0900

    Fix problem video encoder does not support odd resolution

--- liveness/src/main/java/com/amplifyframework/ui/liveness/tada/LivenessCoordinatorTargetParam.kt
+++ liveness/src/main/java/com/amplifyframework/ui/liveness/tada/LivenessCoordinatorTargetParam.kt
@@ -13,11 +13,17 @@ data class LivenessCoordinatorTargetParam(
     val targetEncodeBitrate: Int = (1024 * 1024 * .6).toInt(),
     val targetEncodeKeyFrameInternal: Int = 1,
 ) {
-    val targetWidth: Int = with(density) { targetWidthDp.toPx() }.toInt()
-    val targetHeight: Int = with(density) { targetHeightDp.toPx() }.toInt()
+    val targetWidth: Int = targetWidthDp.toEvenPx()
+    val targetHeight: Int = targetHeightDp.toEvenPx()
     val targetResolutionSize: Size = Size(
         targetWidth,
         targetHeight,
     )
     val targetAspectRatio: Float = targetWidth.toFloat() / targetHeight.toFloat()
+
+    private fun Dp.toEvenPx(): Int {
+        val px = with(density) { toPx() }
+        val rounded = px.toInt()
+        return if (rounded % 2 == 0) rounded else (rounded + 1)
+    }
 }
